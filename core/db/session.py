48c013b808d58339b6d2f230fd5bfcb243788eef
Baohuynh <bao.huynh@tpptechnology.com>
2023-06-26 13:06:53 +0700

    instal sqlalchemy

--- core/db/session.py
+++ core/db/session.py
@@ -1,52 +1,29 @@
-from contextvars import ContextVar, Token
-from typing import Union
-
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    create_async_engine,
-    async_scoped_session,
-)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.sql.expression import Update, Delete, Insert
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from core.config import config
+from sqlalchemy.engine.url import URL
 
-session_context: ContextVar[str] = ContextVar("session_context")
-
-
-def get_session_context() -> str:
-    return session_context.get()
-
-
-def set_session_context(session_id: str) -> Token:
-    return session_context.set(session_id)
-
-
-def reset_session_context(context: Token) -> None:
-    session_context.reset(context)
-
-
-engines = {
-    "writer": create_async_engine(config.WRITER_DB_URL, pool_recycle=3600),
-    "reader": create_async_engine(config.READER_DB_URL, pool_recycle=3600),
-}
-
-
-class RoutingSession(Session):
-    def get_bind(self, mapper=None, clause=None, **kw):
-        if self._flushing or isinstance(clause, (Update, Delete, Insert)):
-            return engines["writer"].sync_engine
-        else:
-            return engines["reader"].sync_engine
-
-
-async_session_factory = sessionmaker(
-    class_=AsyncSession,
-    sync_session_class=RoutingSession,
+url = URL(
+    drivername="mysql+pymysql",
+    username=config.DB_USER,
+    password=config.DB_PASSWORD,
+    host=config.DB_HOST,
+    port=config.DB_PORT,
+    database=config.DB_DATABASE,
 )
-session: Union[AsyncSession, async_scoped_session] = async_scoped_session(
-    session_factory=async_session_factory,
-    scopefunc=get_session_context,
-)
-Base = declarative_base()
+
+# engine = create_engine(url)
+
+# query = "SELECT * FROM users"
+# result = engine.execute(query)
+# for row in result:
+#     # Access the values in each row
+#     id = row["id"]
+#     name = row["username"]
+#     email = row["email"]
+
+#     # Process the values as needed
+#     print(id, name, email)
+
+# # Close the result set
+# result.close()
